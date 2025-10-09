780b8154f61446f54f7c7b2d5f5ce2a0ec689488
riyad1998-cyber <jobiyedriyad@gmail.com>
2025-10-09 12:20:46 +0600

    added my updated tost functionality

--- src/Pages/ApplicationsDetail.jsx
+++ src/Pages/ApplicationsDetail.jsx
@@ -12,6 +12,8 @@ import {
     LabelList,
     ResponsiveContainer
 } from 'recharts';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ApplicationsDetail = () => {
     const { id } = useParams();
@@ -23,7 +25,7 @@ const ApplicationsDetail = () => {
     if (loading) return <p>Loading.....</p>;
     if (!application) return  <div className="col-span-full flex flex-col justify-center items-center h-40 h-150">
         <p className="text-gray-500 text-4xl font-bold mb-4">App Is Not Found!!</p>
-        <p className='text-gray-500 mb-4'>The app you looking for does not exist or has benn removed.</p>
+        <p className='text-gray-500 mb-4'>The app you looking for does not exist or has been removed.</p>
         <Link
           to="/applications"
           className="btn flex items-center justify-center gap-2 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-lg w-[145px] h-12 sm:text-xl text-base max-[425px]:w-[120px] max-[425px]:h-10 max-[425px]:text-sm max-[375px]:w-[110px] max-[375px]:h-9 max-[375px]:text-[13px]"
@@ -45,20 +47,27 @@ const ApplicationsDetail = () => {
         localStorage.setItem('installing', JSON.stringify(updatedList));
 
         setInstalledState(true);
+
+        toast.success(`${companyName} installed successfully!`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        });
     }
 
     return (
         <div>
-         
+            <ToastContainer />
+
             <div className='flex flex-col lg:flex-row w-full max-w-[1700px] mx-auto gap-10 mt-[40px] px-4 sm:px-6'>
-             
                 <figure className='w-full sm:w-[300px] md:w-[350px] h-[300px] sm:h-[300px] md:h-[350px] mx-auto'>
                     <img src={image} alt={title} className="w-full h-full object-cover rounded-md" />
                 </figure>
 
-              
                 <div className='flex-1'>
-                    
                     <div className="w-full border-b-[3px] border-gray-300 mb-6">
                         <div className="pb-3">
                             <h1 className='text-[24px] sm:text-[28px] md:text-[32px] font-bold text-center lg:text-left'>
@@ -70,7 +79,7 @@ const ApplicationsDetail = () => {
                         </div>
                     </div>
 
-          
+     
                     <div className='flex flex-col sm:flex-row  gap-8 mb-6'>
                         <div className='flex flex-col items-center'>
                             <img src={downloadIcon} alt="Downloads" className='w-8 h-8 sm:w-10 sm:h-9 mb-1' />
@@ -89,11 +98,10 @@ const ApplicationsDetail = () => {
                         </div>
                     </div>
 
-                    {/* Install Button */}
                     <div className='flex justify-center lg:justify-start'>
                         <button
                             onClick={hadnleInstalled}
-                            disabled={installedState} // 🟡 
+                            disabled={installedState}
                             className={`px-6 py-3 rounded-lg font-semibold w-full sm:w-[239px] h-[50px] transition 
                                 ${installedState ? 'bg-gray-400 cursor-not-allowed' : 'bg-[#00D390] hover:bg-purple-800 text-white'}`}
                         >
@@ -123,6 +131,7 @@ const ApplicationsDetail = () => {
                 </ResponsiveContainer>
             </div>
 
+            {/* Description */}
             <div className='w-full max-w-[1700px] mx-auto px-4 sm:px-6 lg:px-8 mt-10 mb-10'>
                 <h1 className='text-2xl sm:text-2xl md:text-2xl font-bold'>Description</h1>
                 <p className='mt-4 sm:mt-6 text-gray-500 text-sm sm:text-base md:text-lg'>{description}</p>
